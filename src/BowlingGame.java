b9f89d6016c60d949caac4e70b8705f08ac517ea
somename <someemail>
2017-05-12 12:37:54 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -9,7 +9,7 @@ public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
-	private int bonusBroj = 10;
+	private int bonusBroj = 0;
 	Frame frame = new Frame();
 	
 	
