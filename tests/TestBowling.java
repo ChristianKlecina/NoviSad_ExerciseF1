a8bd8ffe115cca675e32fdbef41f1b2b07dee3eb
somename <someemail>
2017-05-12 12:39:32 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -20,5 +20,15 @@ public class TestBowling {
 		Frame frame = new Frame(8, 2);
 		assertTrue(frame.isSpare());
 	}
+	
+	@Test
+	public void Score(){
+		BowlingGame game = new BowlingGame();
+		Frame frame = new Frame(5,4);
+		game.addFrame(frame);
+		game.score();
+		
+		assertEquals(9,9);
+	}
 
 }
